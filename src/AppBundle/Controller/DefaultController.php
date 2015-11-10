7120edd35301e61e71f6ffe9123f0b4f098de83f
siddhartha.mahata <siddhartha.mahata@gmail.com>
2015-11-11 01:14:42 +0530

    redirect controller added

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -6,6 +6,10 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
 class DefaultController extends Controller
 {
     /**
@@ -16,6 +20,28 @@ class DefaultController extends Controller
 
         return $this->render('default/index.html.twig', array(
             'base_dir' => realpath($this->container->getParameter('kernel.root_dir').'/..'),
+        ));
+    }
+
+    /**
+     * Finds and displays a Url entity.
+     *
+     * @Route("/{shortcode}", name="redirect_shortcode")
+     * @Method("GET")
+     * @Template()
+     */
+    public function redirectAction($shortcode)
+    {
+        $em = $this->getDoctrine()->getManager();
+
+        $entity = $em->getRepository('AppBundle:Url')->findOneByShortcode($shortcode);
+
+        if (!$entity) {
+            throw $this->createNotFoundException('Unable to find Url entity.');
+        }
+
+        return $this->render('default/redirect.html.twig', array(
+            'entity' => $entity,
         ));
     }
 }
